116d85e50c9ffdff2710921c2c75c8a49cf3dcfe
AdrianStr <adrian.strzelczyk@stud.fh-luebeck.de>
2017-06-28 17:56:00 +0200

    first push
    - added interface to DetailActivity

--- app/src/main/java/com/example/avenger/todoapp/activity/DetailActivity.java
+++ app/src/main/java/com/example/avenger/todoapp/activity/DetailActivity.java
@@ -2,5 +2,34 @@ package com.example.avenger.todoapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 
-public class DetailActivity extends AppCompatActivity {
+import com.example.avenger.todoapp.model.Todo;
+import com.example.avenger.todoapp.view.DetailView;
+
+public class DetailActivity extends AppCompatActivity implements DetailView {
+
+
+    @Override
+    public void saveItem() {
+        
+    }
+
+    @Override
+    public void deleteItem() {
+
+    }
+
+    @Override
+    public void setTodo(Todo todo) {
+
+    }
+
+    @Override
+    public Todo getCurrentTodo() {
+        return null;
+    }
+
+    @Override
+    public void displayTodoNotFound() {
+
+    }
 }
