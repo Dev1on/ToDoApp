e9e02fa4b22fcf19f78f4312102cc548a4a91ebd
dev1on <respondek.kevin@gmail.com>
2017-07-02 17:00:58 +0200

    added tab view for maps (no markers yet)

--- app/src/main/java/com/example/avenger/todoapp/activity/FullListMapActivity.java
+++ app/src/main/java/com/example/avenger/todoapp/activity/FullListMapActivity.java
@@ -1,46 +1,93 @@
 package com.example.avenger.todoapp.activity;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.example.avenger.todoapp.R;
+import com.example.avenger.todoapp.database.DBApplication;
+import com.example.avenger.todoapp.model.Todo;
+import com.example.avenger.todoapp.presenter.FullListMapPresenter;
+import com.example.avenger.todoapp.view.FullListMapView;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FullListMapActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
 
+public class FullListMapActivity extends Fragment implements FullListMapView, OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private MapView mMapView;
     private GoogleMap mMap;
+    private FullListMapPresenter presenter;
+    private ArrayList<Todo> todos;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.full_list_map_activity);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.full_list_map_activity, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+
+        presenter = new FullListMapPresenter(this, (DBApplication)getActivity().getApplication());
+        presenter.readAllToDosForInit();
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return rootView;
     }
 
+    @Override
+    public void updateView(ArrayList<Todo> todos) {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    }
+
+    @Override
+    public void displayTodosNotFound() {
+        Toast.makeText(getContext(), "Sorry, Todos not found", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void startDetail(long id) {
+        Intent showTodoDetails = new Intent(getContext(), DetailActivity.class);
+        showTodoDetails.putExtra("id", id);
+        showTodoDetails.putExtra("createItem", false);
+        startActivityForResult(showTodoDetails, 2);
+    }
+
+    @Override
+    public void setTodos(ArrayList<Todo> todos) {
+        this.todos = todos;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Integer todoId = (Integer) marker.getTag();
+        startDetail(todoId);
+
+        return false;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for (Todo todo : todos) {
+            Todo.Location location = todo.getLocation();
+            LatLng latLng = new LatLng(location.getLatlng().getLat(), location.getLatlng().getLng());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(location.getName())).setTag(todo.getId());
+            mMap.setOnMarkerClickListener(this);
+        }
     }
 }
